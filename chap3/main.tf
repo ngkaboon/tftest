954ee158a5b81021d858ed1f889ab283ddb88e1e
Kevin KB Ng <kevinkbng@kevinngaboonmbp.lan>
2020-08-02 00:31:17 +0800

    added use of terragrunt

--- chap3/main.tf
+++ chap3/main.tf
@@ -2,8 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket  = "terraform-up-and-running-ngkaboon"
+    region  = "us-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state"
+  bucket = "terraform-up-and-running-ngkaboon"
 
   versioning {
     enabled = true
@@ -12,4 +20,9 @@ resource "aws_s3_bucket" "terraform_state" {
   lifecycle {
     prevent_destroy = true
   }
+}
+
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
 }
