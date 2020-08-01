78a654d752c53950720132fa08de75ed93157efc
Kevin KB Ng <kevinkbng@kevinngaboonmbp.lan>
2020-08-01 10:49:27 +0800

    change main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+
   tags = {
     Name = "terraform-example"
   }
