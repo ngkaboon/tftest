1ecfa19af308b9c84f4c54cbc1e21708a52a704d
Kevin KB Ng <kevinkbng@kevinngaboonmbp.lan>
2020-08-01 15:05:56 +0800

    Some more changes

--- main.tf
+++ main.tf
@@ -3,18 +3,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
-
-  user_data = <<-EOF
+  ami                    = "ami-40d28157"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 
-
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform_example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
 }
 
