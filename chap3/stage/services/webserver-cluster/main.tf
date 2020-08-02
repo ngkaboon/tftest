4a63c175d0f70554166777cc707b46ed0755806c
Kevin KB Ng <kevinkbng@kevinngaboonmbp.lan>
2020-08-02 22:01:53 +0800

    update with break up to user-data shell

--- chap3/stage/services/webserver-cluster/main.tf
+++ chap3/stage/services/webserver-cluster/main.tf
@@ -6,16 +6,12 @@ terraform {
   backend "s3" {}
 }
 
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-40d28157"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
+  user_data       = data.template_file.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -98,3 +94,24 @@ resource "aws_security_group" "elb" {
 
 data "aws_availability_zones" "all" {}
 
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+
+  config = {
+    bucket = "terraform-up-and-running-ngkaboon"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
+}
