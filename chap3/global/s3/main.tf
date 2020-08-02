9ffa96f616a7022a733331a2379932ec35f641e4
Kevin KB Ng <kevinkbng@kevinngaboonmbp.lan>
2020-08-02 14:24:07 +0800

    refactor to use terragrunt and paths

--- chap3/global/s3/main.tf
+++ chap3/global/s3/main.tf
@@ -3,12 +3,7 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    bucket  = "terraform-up-and-running-ngkaboon"
-    region  = "us-east-1"
-    key     = "terraform.tfstate"
-    encrypt = true
-  }
+  backend "s3" {}
 }
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-up-and-running-ngkaboon"
@@ -23,6 +18,3 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.terraform_state.arn
-}
