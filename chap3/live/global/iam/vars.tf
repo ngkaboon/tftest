c9644bfcaaedabc1c4a8e4bc593ee81d9aec95d6
Kevin KB NG <kevin_kb_ng@tech.gov.sg>
2020-08-09 15:05:44 +0800

    another commit

--- chap3/live/global/iam/vars.tf
+++ chap3/live/global/iam/vars.tf
@@ -1,17 +1,5 @@
-#resource "aws_iam_user" "example" {
-#  count = "${length(var.usernames)}"
-#  name = "${element(var.user_names, count.index}"
-#}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  backend "s3" {}
-}
-
-resource "aws_iam_user" "example" {
-  count = 3
-  name  = "neo.${count.index}"
+variable "user_names" {
+  description = "Create iam users with these names"
+  type        = "list"
+  default     = ["neo", "trinity", "morpheus"]
 }
