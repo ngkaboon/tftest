ef2be43b075fd13c8a47f4af97ca4b06021d3367
Kevin KB NG <kevin_kb_ng@tech.gov.sg>
2020-08-09 15:18:55 +0800

    wrapping up iam

--- chap3/live/global/iam/main.tf
+++ chap3/live/global/iam/main.tf
@@ -1,7 +1,3 @@
-#resource "aws_iam_user" "example" {
-#  count = "${length(var.usernames)}"
-#  name = "${element(var.user_names, count.index}"
-#}
 
 provider "aws" {
   region = "us-east-1"
@@ -11,7 +7,31 @@ terraform {
   backend "s3" {}
 }
 
+#resource "aws_iam_user" "example" {
+#  count = 3
+#  name  = "neo.${count.index}"
+#}
+
+data "aws_iam_policy_document" "ec2_read_only" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ec2_read_only" {
+  name   = "ec2-read-only"
+  policy = data.aws_iam_policy_document.ec2_read_only.json
+}
+
 resource "aws_iam_user" "example" {
-  count = 3
-  name  = "neo.${count.index}"
+  count = length(var.user_names)
+  name  = element(var.user_names, count.index)
+}
+
+resource "aws_iam_user_policy_attachment" "ec2_access" {
+  count      = length(var.user_names)
+  user       = element(aws_iam_user.example.*.name, count.index)
+  policy_arn = aws_iam_policy.ec2_read_only.arn
 }
--- chap3/live/global/iam/vars.tf
+++ chap3/live/global/iam/vars.tf
@@ -1,5 +1,5 @@
 variable "user_names" {
   description = "Create iam users with these names"
-  type        = "list"
+  type        = list(string)
   default     = ["neo", "trinity", "morpheus"]
 }
